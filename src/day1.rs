6868616ec35a64425dc3e98ffe2945e7d0aab055
Wilfried Chauveau <wilfried.chauveau@ithinuel.me>
2020-12-02 20:18:57 +0000

    initial commit

---
+++ src/day1.rs
@@ -0,0 +1,41 @@
+use std::io::stdin;
+use std::io::BufRead;
+
+fn look_up(base: u32, data: &Vec<u32>) -> Option<u32> {
+    data.iter()
+        .find(|&a| {
+            if base < *a {
+                return false;
+            }
+            let b = base - a;
+            data.contains(&b)
+        })
+        .copied()
+}
+
+fn main() {
+    let input = stdin();
+    let mut data: Vec<u32> = input
+        .lock()
+        .lines()
+        .filter_map(|s| s.ok())
+        .filter_map(|s| s.parse().ok())
+        .collect();
+
+    data.sort();
+    if let Some(a) = look_up(2020, &data) {
+        let b = 2020 - a;
+        println!("{}*{}={}", a, b, a * b);
+    } else {
+        println!("nomatch");
+    }
+
+    for a in &data {
+        let aa = 2020 - a;
+        if let Some(b) = look_up(aa, &data) {
+            let c = aa - b;
+            println!("{}*{}*{}={}", a, b, c, a * b * c);
+            break;
+        }
+    }
+}
