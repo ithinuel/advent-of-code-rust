37315ec5804e9c6bd8df4339863c4cd1f642ac03
Wilfried Chauveau <wilfried.chauveau@ithinuel.me>
2021-12-10 07:12:53 +0000

    day10 part2

--- src/day10.rs
+++ src/day10.rs
@@ -1,26 +1,34 @@
 use aoc_runner_derive::*;
+use either::Either::{self, *};
+use itertools::Itertools;
+
+fn check_line(line: &str) -> Result<(), Either<u8, Vec<u8>>> {
+    let mut stack = Vec::new();
+    for b in line.bytes() {
+        if let b'(' | b'<' | b'{' | b'[' = b {
+            stack.push(b);
+        } else {
+            let is_ok = stack.pop().map(|b2| match (b2, b) {
+                (b'(', b')') | (b'{', b'}') | (b'[', b']') | (b'<', b'>') => true,
+                _ => false,
+            });
+            if !is_ok.unwrap_or(true) {
+                return Err(Left(b));
+            }
+        }
+    }
+    if !stack.is_empty() {
+        Err(Right(stack))
+    } else {
+        Ok(())
+    }
+}
 
 #[aoc(day10, part1)]
 fn part1(input: &str) -> usize {
     input
         .lines()
-        .filter_map(|l| {
-            let mut stack = Vec::new();
-            for b in l.bytes() {
-                if let b'(' | b'<' | b'{' | b'[' = b {
-                    stack.push(b);
-                } else {
-                    let is_ok = stack.pop().map(|b2| match (b2, b) {
-                        (b'(', b')') | (b'{', b'}') | (b'[', b']') | (b'<', b'>') => true,
-                        _ => false,
-                    });
-                    if !is_ok.unwrap_or(true) {
-                        return Some(b);
-                    }
-                }
-            }
-            None
-        })
+        .filter_map(|l| check_line(l).err().and_then(|e| e.left()))
         .map(|c| match c {
             b')' => 3,
             b']' => 57,
@@ -29,6 +37,38 @@ fn part1(input: &str) -> usize {
             _ => unreachable!(),
         })
         .sum()
+}
+
+#[aoc(day10, part2)]
+fn part2(input: &str) -> usize {
+    let mut scores = input
+        .lines()
+        .filter_map(|line| {
+            check_line(line).err().and_then(|e| e.right()).map(|stack| {
+                stack.into_iter().rev().map(|b| match b {
+                    b'(' => b')',
+                    b'{' => b'}',
+                    b'[' => b']',
+                    b'<' => b'>',
+                    _ => unreachable!(),
+                })
+            })
+        })
+        .map(|end| {
+            end.fold(0, |acc, b| {
+                acc * 5
+                    + match b {
+                        b')' => 1,
+                        b']' => 2,
+                        b'}' => 3,
+                        b'>' => 4,
+                        _ => unreachable!(),
+                    }
+            })
+        })
+        .collect_vec();
+    scores.sort_unstable();
+    scores[scores.len() / 2]
 }
 
 #[cfg(test)]
@@ -47,5 +87,10 @@ mod test {
     #[test]
     fn part1() {
         assert_eq!(26397, super::part1(EXAMPLE));
+    }
+
+    #[test]
+    fn part2() {
+        assert_eq!(288957, super::part2(EXAMPLE));
     }
 }
