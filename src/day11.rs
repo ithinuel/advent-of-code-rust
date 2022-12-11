b40c5b134b74b580a050011f11f19f177f2751f9
Wilfried Chauveau <wilfried.chauveau@ithinuel.me>
2022-12-11 08:39:11 +0000

    Day11: refactor to use the actual input

--- src/day11.rs
+++ src/day11.rs
@@ -1,73 +1,86 @@
+use itertools::Itertools;
 use yaah::{aoc, aoc_generator};
 
-#[derive(Clone)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum MulOp {
+    Imm(usize),
+    Old,
+}
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum InspectOp {
+    Add(usize),
+    Mul(MulOp),
+}
+impl InspectOp {
+    fn inspect(&self, v: usize) -> usize {
+        match *self {
+            InspectOp::Add(imm) => v + imm,
+            InspectOp::Mul(MulOp::Imm(imm)) => v * imm,
+            InspectOp::Mul(MulOp::Old) => v * v,
+        }
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
 pub struct Monkey {
     items: Vec<usize>,
-    inspect: fn(usize) -> usize,
-    check: fn(usize) -> usize,
+    inspect: InspectOp,
+    check: (usize, usize, usize),
 }
 
 #[aoc_generator(day11)]
-fn day11(_: &'static str) -> Vec<Monkey> {
-    vec![
-        Monkey {
-            items: vec![62, 92, 50, 63, 62, 93, 73, 50],
-            inspect: |v| (v * 7) % 9699690,
-            check: |v| if (v % 2) == 0 { 7 } else { 1 },
-        },
-        Monkey {
-            items: vec![51, 97, 74, 84, 99],
-            inspect: |v| v + 3,
-            check: |v| if (v % 7) == 0 { 2 } else { 4 },
-        },
-        Monkey {
-            items: vec![98, 86, 62, 76, 51, 81, 95],
-            inspect: |v| v + 4,
-            check: |v| if (v % 13) == 0 { 5 } else { 4 },
-        },
-        Monkey {
-            items: vec![53, 95, 50, 85, 83, 72],
-            inspect: |v| v + 5,
-            check: |v| if (v % 19) == 0 { 6 } else { 0 },
-        },
-        Monkey {
-            items: vec![59, 60, 63, 71],
-            inspect: |v| (v * 5) % 9699690,
-            check: |v| if (v % 11) == 0 { 5 } else { 3 },
-        },
-        Monkey {
-            items: vec![92, 65],
-            inspect: |v| (v * v) % 9699690,
-            check: |v| if (v % 5) == 0 { 6 } else { 3 },
-        },
-        Monkey {
-            items: vec![78],
-            inspect: |v| v + 8,
-            check: |v| if (v % 3) == 0 { 0 } else { 7 },
-        },
-        Monkey {
-            items: vec![84, 93, 54],
-            inspect: |v| v + 1,
-            check: |v| if (v % 17) == 0 { 2 } else { 1 },
-        },
-    ]
+fn day11(input: &'static str) -> Vec<Monkey> {
+    input
+        .split("\n\n")
+        .filter_map(|monkey| {
+            let mut lines = monkey.lines().skip(1);
+            let items = lines
+                .next()?
+                .split(':')
+                .skip(1)
+                .flat_map(|v| v.trim().split(", "))
+                .filter_map(|v| v.parse().ok())
+                .collect_vec();
+
+            let (operand, operator) = lines.next()?.split(' ').rev().take(2).collect_tuple()?;
+            let inspect = match (operator, operand) {
+                ("+", imm) => InspectOp::Add(imm.parse().ok()?),
+                ("*", "old") => InspectOp::Mul(MulOp::Old),
+                ("*", imm) => InspectOp::Mul(MulOp::Imm(imm.parse().ok()?)),
+                _ => unreachable!(),
+            };
+
+            let check = lines
+                .filter_map(|l| l.split(' ').last()?.parse().ok())
+                .collect_tuple()?;
+            Some(Monkey {
+                items,
+                inspect,
+                check,
+            })
+        })
+        .collect()
 }
 
 #[aoc(day11, part1)]
-fn day11_part1(monkeys: &Vec<Monkey>) -> usize {
-    let mut monkeys = monkeys.clone();
+fn day11_part1(monkeys: &[Monkey]) -> usize {
+    let mut monkeys = monkeys.to_vec();
     let mut monkey_businesses = vec![0; monkeys.len()];
 
     for _round in 0..20 {
         for monkey_id in 0..monkeys.len() {
-            let items = std::mem::replace(&mut monkeys[monkey_id].items, Vec::new());
+            let items = std::mem::take(&mut monkeys[monkey_id].items);
             monkey_businesses[monkey_id] += items.len();
 
             items.into_iter().for_each(|v| {
                 let monkey = &monkeys[monkey_id];
-                let mut new = (monkey.inspect)(v);
+                let mut new = monkey.inspect.inspect(v);
                 new /= 3;
-                let target = (monkey.check)(new);
+                let target = if (new % monkey.check.0) == 0 {
+                    monkey.check.1
+                } else {
+                    monkey.check.2
+                };
                 monkeys[target].items.push(new);
             });
         }
@@ -77,19 +90,26 @@ fn day11_part1(monkeys: &Vec<Monkey>) -> usize {
 }
 
 #[aoc(day11, part2)]
-fn day11_part2(monkeys: &Vec<Monkey>) -> usize {
-    let mut monkeys = monkeys.clone();
+fn day11_part2(monkeys: &[Monkey]) -> usize {
+    let mut monkeys = monkeys.to_vec();
     let mut monkey_businesses = vec![0; monkeys.len()];
+
+    let master_mod: usize = monkeys.iter().map(|m| m.check.0).product();
 
     for _round in 0..10000 {
         for monkey_id in 0..monkeys.len() {
-            let items = std::mem::replace(&mut monkeys[monkey_id].items, Vec::new());
+            let items = std::mem::take(&mut monkeys[monkey_id].items);
             monkey_businesses[monkey_id] += items.len();
 
             items.into_iter().for_each(|v| {
                 let monkey = &monkeys[monkey_id];
-                let new = (monkey.inspect)(v);
-                let target = (monkey.check)(new);
+                let mut new = monkey.inspect.inspect(v);
+                new %= master_mod;
+                let target = if (new % monkey.check.0) == 0 {
+                    monkey.check.1
+                } else {
+                    monkey.check.2
+                };
                 monkeys[target].items.push(new);
             });
         }
@@ -101,37 +121,73 @@ fn day11_part2(monkeys: &Vec<Monkey>) -> usize {
 #[cfg(test)]
 mod test {
     use crate::day11::Monkey;
-    fn input() -> Vec<Monkey> {
+    const EXAMPLE: &str = r"Monkey 0:
+  Starting items: 79, 98
+  Operation: new = old * 19
+  Test: divisible by 23
+    If true: throw to monkey 2
+    If false: throw to monkey 3
+
+Monkey 1:
+  Starting items: 54, 65, 75, 74
+  Operation: new = old + 6
+  Test: divisible by 19
+    If true: throw to monkey 2
+    If false: throw to monkey 0
+
+Monkey 2:
+  Starting items: 79, 60, 97
+  Operation: new = old * old
+  Test: divisible by 13
+    If true: throw to monkey 1
+    If false: throw to monkey 3
+
+Monkey 3:
+  Starting items: 74
+  Operation: new = old + 3
+  Test: divisible by 17
+    If true: throw to monkey 0
+    If false: throw to monkey 1
+";
+
+    fn example_monkeys() -> Vec<Monkey> {
+        use super::InspectOp::*;
+        use super::MulOp::*;
         vec![
             Monkey {
                 items: vec![79, 98],
-                inspect: |v| (v * 19) % 96577,
-                check: |v| if (v % 23) == 0 { 2 } else { 3 },
+                inspect: Mul(Imm(19)),
+                check: (23, 2, 3),
             },
             Monkey {
                 items: vec![54, 65, 75, 74],
-                inspect: |v| v + 6,
-                check: |v| if (v % 19) == 0 { 2 } else { 0 },
+                inspect: Add(6),
+                check: (19, 2, 0),
             },
             Monkey {
                 items: vec![79, 60, 97],
-                inspect: |v| (v * v) % 96577,
-                check: |v| if (v % 13) == 0 { 1 } else { 3 },
+                inspect: Mul(Old),
+                check: (13, 1, 3),
             },
             Monkey {
                 items: vec![74],
-                inspect: |v| v + 3,
-                check: |v| if (v % 17) == 0 { 0 } else { 1 },
+                inspect: Add(3),
+                check: (17, 0, 1),
             },
         ]
     }
 
     #[test]
+    fn day11_gen() {
+        assert_eq!(example_monkeys(), super::day11(EXAMPLE));
+    }
+
+    #[test]
     fn day11_part1() {
-        assert_eq!(10605, super::day11_part1(&input()));
+        assert_eq!(10605, super::day11_part1(&example_monkeys()));
     }
     #[test]
     fn day11_part2() {
-        assert_eq!(2713310158, super::day11_part2(&input()));
+        assert_eq!(2713310158, super::day11_part2(&example_monkeys()));
     }
 }
